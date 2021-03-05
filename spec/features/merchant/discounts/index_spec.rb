2d3a1568ea9fb3be8e66455da62e66b203c17ceb
Wil McCauley <wmccauley724@gmail.com>
2021-03-04 17:09:56 -0700

    Add index view spec to merchant discount spec features

---
+++ spec/features/merchant/discounts/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'when I visit the merchant discount index page' do
+  before :each do
+    @merchant1 = create(:merchant)
+
+    @disco1 = @merchant1.discounts.create!(percentage: 10, threshold: 12)
+  end
+  it 'displays discounts with percentage and threshold' do
+    visit merchant_discounts_path(@merchant1)
+
+    within("#discount-#{@disco1.id}") do
+      expect(page).to have_content(@disco1.percentage)
+      expect(page).to have_content(@disco1.threshold)
+      expect(page).to have_link("Discount Info Page")
+    end
+  end
+  it 'displays the next 3 upcoming holidays in a holidays section'
+end
