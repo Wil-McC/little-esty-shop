1bc983f940214d3d164199d206990d236e19d12e
Wil McCauley <wmccauley724@gmail.com>
2021-03-05 11:53:01 -0700

    Complete date api consumption with feature testing

---
+++ app/services/date_service.rb
@@ -0,0 +1,22 @@
+# require "json"
+# require "faraday"
+
+class DateService
+
+  def self.upcoming
+    get_data('https://date.nager.at/Api/v2/NextPublicHolidays/US')
+  end
+
+  def self.upcoming_three
+    out = upcoming.first(3)
+  end
+
+  def self.get_data(url)
+    response = Faraday.get(url)
+    # do |req|
+      # req.headers['Authorization'] = "token #{ENV['github_token']}"
+    # end
+    data = response.body
+    JSON.parse(data, symbolize_names: true)
+  end
+end
