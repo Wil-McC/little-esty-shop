01b34fb24ee6cb4a086f02b4c4a0b13e91cb8778
Wil McCauley <wmccauley724@gmail.com>
2021-03-04 17:06:53 -0700

    Add model test for discount one to many relationship

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe Discount, type: :model do
   describe 'validations' do
     it { should validate_presence_of :percentage }
     it { should validate_presence_of :threshold }
-
   end
+  
   describe 'relationships' do
-    it { should  }
-    it { should  }
+    it { should belong_to :merchant }
   end
 end
